ce6c077e34a0cfcdcb52645ab4627c6ff3704431
juliettesav <juliettesavarese@gmail.com>
2025-02-10 14:03:36 -0600

    sql practice

--- SQL_practice.sql
+++ SQL_practice.sql
@@ -3,3 +3,15 @@ FROM users;
 
 SELECT species 
 FROM animals;
+
+SELECT name, email 
+FROM users;
+
+SELECT * --(an asterisk selects all)
+FROM animals; 
+
+SELECT country --(all countries, including duplicates)
+FROM students; 
+
+SELECT DISTINCT country --(all countries, excluding duplicates)
+FROM students; 
